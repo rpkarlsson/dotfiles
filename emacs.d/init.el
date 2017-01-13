04a485b0c7968fd1b94f08cd79ac5d2ec96791f4
Robin Karlsson <robinpkarlsson@gmail.com>
2017-01-13 20:58:33 +0100

    Cleanup init.el

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,55 +1,13 @@
-;;; init.el --- Configuration of Emacs
-;;
-;; (c) 2000 - 2016 Arjen Wiersma
-;;
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
-
-;; Define package repositories
 (require 'package)
-;; (add-to-list 'package-archives
-;;              '("marmalade" . "http://marmalade-repo.org/packages/") t)
-;; (add-to-list 'package-archives
-;;              '("tromey" . "http://tromey.com/elpa/") t)
 (add-to-list 'package-archives
                           '("melpa" . "http://melpa.milkbox.net/packages/") t)
 
-;; (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-;;                          ("marmalade" .
-;;                          "http://marmalade-repo.org/packages/")
-;;                          ("melpa" .
-;;                          "http://melpa-stable.milkbox.net/packages/")))
-
-
 (package-initialize)
 
-;; Download the ELPA archive description if needed.
-;; This informs Emacs about the latest versions of all packages, and
-;; makes them available for download.
 (when (not package-archive-contents)
     (package-refresh-contents))
-;; The packages you want installed. You can also install these
-;; manually with M-x package-install
-;; Add in your own as you wish:
 (defvar my-packages
-  '(
-
-    ;; allow ido usage in as many contexts as possible. see
-    ;; customizations/navigation.el line 23 for a description
-    ;; of ido
-    ido-ubiquitous
-
-    ;; Enhances M-x to allow easier execution of commands. Provides
-    ;; a filterable list of possible commands in the minibuffer
-    ;; http://www.emacswiki.org/emacs/Smex
-    smex
-
-    ;; edit html tags like sexps
-    tagedit
-
-    ace-window
+  '(ace-window
     arjen-grey-theme
     cider
     clojure-mode
@@ -57,11 +15,13 @@
     editorconfig
     evil
     evil-surround
+    ido-ubiquitous
     magit
     org
     paredit
-    projectile))
-
+    projectile
+    smex
+    tagedit))
 
 
 ;; On OS X, an Emacs instance started from the graphical user
@@ -74,7 +34,7 @@
 ;; https://github.com/purcell/exec-path-from-shell
 (if (eq system-type 'darwin)
     (add-to-list 'my-packages 'exec-path-from-shell))
-
+1
 (dolist (p my-packages)
   (when (not (package-installed-p p))
     (package-install p)))
@@ -84,19 +44,3 @@
 (require 'ob-tangle)
 (setq init-dir (file-name-directory (or load-file-name (buffer-file-name))))
 (org-babel-load-file (expand-file-name "loader.org" init-dir))
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(coffee-tab-width 2)
- '(package-selected-packages
-   (quote
-    (projectile paredit magit evil-surround evil clojure-mode-extra-font-locking cider tagedit smex ido-ubiquitous exec-path-from-shell))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
