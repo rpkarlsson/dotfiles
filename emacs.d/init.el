f59b7cef51e6ad0bba831710b61636c96c49d23b
Robin Karlsson <robinpkarlsson@gmail.com>
2017-01-14 09:30:20 +0100

    Remove unneccessary osx/shell fix

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,3 +1,4 @@
+;; Install packages
 (require 'package)
 (add-to-list 'package-archives
                           '("melpa" . "http://melpa.milkbox.net/packages/") t)
@@ -6,6 +7,7 @@
 
 (when (not package-archive-contents)
   (package-refresh-contents))
+
 (defvar my-packages
   '(ace-window
      arjen-grey-theme
@@ -24,23 +26,12 @@
      smex
      tagedit))
 
-
-;; On OS X, an Emacs instance started from the graphical user
-;; interface will have a different environment than a shell in a
-;; terminal window, because OS X does not run a shell during the
-;; login. Obviously this will lead to unexpected results when
-;; calling external utilities like make from Emacs.
-;; This library works around this problem by copying important
-;; environment variables from the user's shell.
-;; https://github.com/purcell/exec-path-from-shell
-(if (eq system-type 'darwin)
-    (add-to-list 'my-packages 'exec-path-from-shell))
-
 (dolist (p my-packages)
   (when (not (package-installed-p p))
     (package-install p)))
 
 
+;; Load the remaining config specified in org-mode
 (require 'org)
 (require 'ob-tangle)
 (setq init-dir (file-name-directory (or load-file-name (buffer-file-name))))
