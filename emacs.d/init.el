20f2a0aebc313c6d01c469e4d87438edb017eebd
Robin Karlsson <robin.karlsson@fortnox.se>
2017-04-04 16:02:32 +0200

    Update

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -41,4 +41,20 @@
 (require 'ob-tangle)
 (setq init-dir (file-name-directory (or load-file-name (buffer-file-name))))
 (org-babel-load-file (expand-file-name "loader.org" init-dir))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+  '(package-selected-packages
+     (quote
+       (web-mode js2-mode htmlize ox-reveal less-css-mode flycheck-clojure flycheck ## ag exec-path-from-shell tagedit smex projectile paredit ido-ubiquitous evil-surround evil-magit editorconfig clojure-mode-extra-font-locking cider arjen-grey-theme ace-window))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
 (put 'dired-find-alternate-file 'disabled nil)
