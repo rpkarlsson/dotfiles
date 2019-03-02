024e4be642ceb70f5d2c868bb179426c463796b2
Robin Karlsson <robinpkarlsson@gmail.com>
2019-03-02 10:00:34 +0100

    More emacs packages and config

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -17,6 +17,10 @@ There are two things you can do about this warning:
 
 (package-initialize)
 
+;;; Notes
+;; Do I need hippieexpand?
+;;
+
 (eval-when-compile
   ;; Following line is not needed if use-package.el is in ~/.emacs.d
   (add-to-list 'load-path "<path where use-package is installed>")
@@ -25,7 +29,27 @@ There are two things you can do about this warning:
 ;;
 ;; Packages
 ;;
+(use-package ace-window
+  :ensure t
+  :config
+  (global-set-key (kbd "C-x o") 'ace-window))
+
 (use-package better-defaults
+  :ensure t)
+
+(use-package cider
+  :ensure t
+  :config
+  (setq cider-repl-pop-to-buffer-on-connect t)
+  (setq cider-repl-pop-to-buffer-on-connect t)
+  (setq cider-repl-pop-to-buffer-on-connect t)
+  (setq cider-repl-pop-to-buffer-on-connect t)
+  (add-to-list 'auto-mode-alist '("\\.edn$" . clojure-mode))
+  (add-to-list 'auto-mode-alist '("\\.boot$" . clojure-mode))
+  ;; (add-to-list 'auto-mode-alist '("\\.cljs.*$" . clojure-mode))
+  (add-hook 'clojure-mode-hook 'enable-paredit-mode))
+
+(use-package clojure-mode-extra-font-locking
   :ensure t)
 
 (use-package evil
@@ -50,6 +74,11 @@ There are two things you can do about this warning:
   :config
   (global-evil-surround-mode 1))
 
+(use-package ido-completing-read+
+  :ensure t
+  :config
+  (ido-ubiquitous-mode 1))
+
 (use-package ido-vertical-mode
   :config
   (ido-vertical-mode 1)
@@ -61,13 +90,61 @@ There are two things you can do about this warning:
 (use-package ob-shell
   :after evil)
 
+(defun org-custom-keys ()
+  "Use n/p instead of arrow keys for moving items since they are unbinded."
+  (local-set-key (kbd "M-n") #'org-metadown)
+  (local-set-key (kbd "M-p") #'org-metaup))
+
 (use-package org
   :ensure org-plus-contrib
-  :config)
-
+  :config
+  (add-hook 'org-mode-hook (lambda ()
+                             ;(org-bullets-mode 1)
+                             (auto-fill-mode 1)
+                             (setq org-src-fontify-natively t)
+                             (org-custom-keys))))
 
 (use-package paredit
   :ensure t)
+
+(use-package projectile
+  :ensure t
+  :config
+  (projectile-mode +1)
+  (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
+  (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+  ;(projectile-global-mode)
+  )
+
+(use-package uniquify
+  :config
+  (setq uniquify-buffer-name-style 'forward))
+
+(use-package smex
+  :ensure t)
+
+(use-package slime
+  :ensure t
+  :config
+  (setq inferior-lisp-program "/usr/bin/sbcl")
+  (slime-setup '(slime-asdf
+                 slime-autodoc
+                 slime-editing-commands
+                 slime-fancy-inspector
+                 slime-fontifying-fu
+                 slime-fuzzy
+                 slime-indentation
+                 slime-mdot-fu
+                 slime-package-fu
+                 slime-references
+                 slime-repl
+                 slime-sbcl-exts
+                 slime-scratch
+                 slime-xref-browser))
+  (slime-autodoc-mode)
+  (setq slime-complete-symbol*-fancy t)
+  (setq slime-complete-symbol-function
+        'slime-fuzzy-complete-symbol))
 
 ;;
 ;; Backups
@@ -83,6 +160,22 @@ There are two things you can do about this warning:
 (set-keyboard-coding-system 'utf-8)
 (set-selection-coding-system 'utf-8)
 (prefer-coding-system 'utf-8)
+
+;;
+;; Calendar
+;;
+(setq calendar-week-start-day 1)
+
+; Add week numbers
+(copy-face font-lock-constant-face 'calendar-iso-week-face)
+(set-face-attribute 'calendar-iso-week-face nil :height 0.7)
+(setq calendar-intermonth-text
+      '(propertize
+        (format "%2d"
+                (car
+                 (calendar-iso-from-absolute
+                  (calendar-absolute-from-gregorian (list month day year)))))
+        'font-lock-face 'calendar-iso-week-face))
 
 ;;
 ;; Lisp
@@ -102,23 +195,29 @@ There are two things you can do about this warning:
 (add-hook 'ielm-mode-hook 'turn-on-eldoc-mode)
 
 ;;
-;; GUI
+;; MISC
 ;;
 (blink-cursor-mode 0)
 (setq ring-bell-function 'ignore)
 (setq inhibit-splash-screen t)
-(set-face-attribute 'default nil :height 140)
+(set-face-attribute 'default nil :height 110)
 (fset 'yes-or-no-p 'y-or-n-p)
 
 (setq vc-follow-symlinks t)
 
-;; Save clipboard strings into kill ring before replacing them. When one selects something in another program to paste it into Emacs, but kills something in Emacs before actually pasting it, this selection is gone unless this variable is non-nil
-(setq save-interprogram-paste-before-kill t)
+(setq create-lockfiles nil)
 
 (global-set-key (kbd "S-C-<left>") 'shrink-window-horizontally)
 (global-set-key (kbd "S-C-<right>") 'enlarge-window-horizontally)
 (global-set-key (kbd "S-C-<down>") 'shrink-window)
 (global-set-key (kbd "S-C-<up>") 'enlarge-window)
+
+;Nov.el
+(add-to-list 'auto-mode-alist '("\\.epub\\'" . nov-mode))
+(setq nov-text-width 80)
+
+;; Save clipboard strings into kill ring before replacing them. When one selects something in another program to paste it into Emacs, but kills something in Emacs before actually pasting it, this selection is gone unless this variable is non-nil
+(setq save-interprogram-paste-before-kill t)
 
 ;; Ido
 (ido-mode t)
@@ -126,11 +225,6 @@ There are two things you can do about this warning:
 (setq ido-use-filename-at-point nil)
 (setq ido-auto-merge-work-directories-length -1)
 ;; (setq ido-use-virtual-buffers t)
-
-(use-package ido-completing-read+
-  :ensure t
-  :config
-  (ido-ubiquitous-mode 1))
 
 ;; Keys
 (defun toggle-comment-on-line ()
@@ -150,7 +244,7 @@ There are two things you can do about this warning:
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (org-plus-contrib evil-magit ido-completing-read+ ido-vertical-mode magit better-defaults evil-surround evil-collection 0blayout intero haskell-mode paredit use-package evil))))
+    (ace-window smex clojure-mode-extra-font-locking uniquify cider slime projectile nov org-plus-contrib evil-magit ido-completing-read+ ido-vertical-mode magit better-defaults evil-surround evil-collection 0blayout intero haskell-mode paredit use-package evil))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
